92f2435837cc4c962bc60c3dba4328de7ed0da0f
Sulkhi <sulkhi25@gmail.co>
2022-06-12 21:14:56 +0700

    belajar promise

--- promise.js
+++ promise.js
@@ -1,7 +1,12 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emo) => {
+  const c = (await promiseTheaterIXX()).concat(await promiseTheaterVGC());
+  return new Promise((r) => {
+    r(c.filter((x) => x.hasil == emo).length);
+  });
+};
 
 module.exports = {
   promiseOutput,
